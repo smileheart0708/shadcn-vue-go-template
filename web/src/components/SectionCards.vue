5cc6f9b10a6ec85c978e47f3e4a14b41352e892d
Smileheart <smileheart0708@163.com>
2026-03-12 18:45:25 +0800

    feat: remove gradient from cards, keep shadow

--- web/src/components/SectionCards.vue
+++ web/src/components/SectionCards.vue
@@ -13,7 +13,7 @@ import {
 </script>
 
 <template>
-  <div class="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+  <div class="*:data-[slot=card]: grid grid-cols-1 gap-4 px-4 lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
     <Card class="@container/card">
       <CardHeader>
         <CardDescription>Total Revenue</CardDescription>
