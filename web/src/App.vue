00cd22062e47d588c6e6edbe7c8c249606f43bf5
Smileheart <smileheart0708@163.com>
2026-03-12 19:30:14 +0800

    feat: add dashboard view with router

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import AppSidebar from '@/components/AppSidebar.vue'
 import SiteHeader from '@/components/SiteHeader.vue'
-import SectionCards from '@/components/SectionCards.vue'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
@@ -11,12 +11,7 @@ import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
       <AppSidebar class="hidden lg:block" />
       <SidebarInset>
         <SiteHeader />
-        <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div class="flex items-center justify-between">
-            <h1 class="text-2xl font-bold tracking-tight">Dashboard</h1>
-          </div>
-          <SectionCards />
-        </main>
+        <RouterView />
       </SidebarInset>
     </div>
   </SidebarProvider>
