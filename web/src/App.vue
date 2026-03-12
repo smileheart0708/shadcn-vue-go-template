0b157fb84d2fc94858843af4db37bd8188d671ff
Smileheart <smileheart0708@163.com>
2026-03-12 16:49:22 +0800

    feat(web): 添加仪表盘组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AppSidebar from '@/components/AppSidebar.vue
+import AppSidebar from '@/components/AppSidebar.vue'
 import SiteHeader from '@/components/SiteHeader.vue'
 import SectionCards from '@/components/SectionCards.vue'
 import { SidebarInset } from '@/components/ui/sidebar'
