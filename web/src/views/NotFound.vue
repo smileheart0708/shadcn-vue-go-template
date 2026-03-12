e30d283ad58af348cf41d11374b38587cf9504d4
Smileheart <smileheart0708@163.com>
2026-03-12 19:34:57 +0800

    fix: restore template in NotFound.vue

--- web/src/views/NotFound.vue
+++ web/src/views/NotFound.vue
@@ -12,3 +12,24 @@ import {
 
 const router = useRouter()
 </script>
+
+<template>
+  <div class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+    <Card class="mx-auto mt-20 max-w-md">
+      <CardHeader>
+        <CardTitle>404</CardTitle>
+        <CardDescription>Page Not Found</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p class="text-sm text-muted-foreground">
+          The page you are looking for does not exist.
+        </p>
+      </CardContent>
+      <CardFooter>
+        <Button @click="router.back()">
+          Go Back
+        </Button>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
